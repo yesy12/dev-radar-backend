6fa34b87d2e37509d6bbb6d145acc0e5fa23384e
Alison <alisonvieira.av29@gmail.com>
2020-01-15 19:47:50 -0300

    salvando no mongodb

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,28 @@
 const { Router } =  require("express");
 const axios = require("axios");
+const Dev = require("./models/Dev");
 
 const routes = Router();
 
-routes.post("/devs/", (req,res)=>{
-    const { github_username } = req.body;
+routes.post("/devs/", async (req,res)=>{
+    const { github_username,techs } = req.body;
 
+    const link = `https://api.github.com/users/${github_username}`;
+    const response = await axios.get(link);
 
+    let { name = login, avatar_url, bio } = response.data;
 
-    return res.json({
-        message: "Hello Omnistack"
-    });
+    const techsArray = techs.split(",").map(tech => tech.trim())
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs : techsArray
+    })
+
+    return res.json(dev);
 })
 
 module.exports = routes;
