c4a2bd5a8e56590d9c22aad08e56b6a5ca3f8401
Alison <alisonvieira.av29@gmail.com>
2020-01-16 15:25:52 -0300

    unique no model

--- src/models/Dev.js
+++ src/models/Dev.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const DevSchema = new mongoose.Schema({
     name: String,
-    github_username: String,
+    github_username: {
+        type: String,
+        unique: true
+    },
     bio: String,
     avatar_url: String,
     techs: [String]
