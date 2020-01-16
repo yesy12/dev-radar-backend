6151d4917b13482e7728a5e53c50a6d799817d46
Alison <alisonvieira.av29@gmail.com>
2020-01-16 19:26:54 -0300

    adicionado cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express();
 
 const routes = require("./routes");
@@ -21,6 +23,7 @@ mongoose.connect(link,{
     console.log(`Error on mongodb: ${error}`)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use("/",routes);
 
