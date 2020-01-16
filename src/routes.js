6a9734b6cfadd4cb32b350e6e5803a1db50fbddd
Alison <alisonvieira.av29@gmail.com>
2020-01-16 15:28:38 -0300

    alterando routes

--- src/routes.js
+++ src/routes.js
@@ -7,22 +7,32 @@ const routes = Router();
 routes.post("/devs/", async (req,res)=>{
     const { github_username,techs } = req.body;
 
-    const link = `https://api.github.com/users/${github_username}`;
-    const response = await axios.get(link);
+    const usuarioExiste = await Dev.findOne({github_username})
 
-    let { name = login, avatar_url, bio } = response.data;
+    if(usuarioExiste){
+        return res.json({
+            "error" : "Dev já esta cadastrado"
+        })
+    }
+    else{
 
-    const techsArray = techs.split(",").map(tech => tech.trim())
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs : techsArray
-    })
-
-    return res.json(dev);
+        const link = `https://api.github.com/users/${github_username}`;
+        const response = await axios.get(link);
+    
+        let { name = login, avatar_url, bio } = response.data;
+    
+        const techsArray = techs.split(",").map(tech => tech.trim())
+    
+        const dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs : techsArray
+        })
+    
+        return res.json(dev);
+    }
 })
 
 module.exports = routes;
